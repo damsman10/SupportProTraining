e0ae67cf52268d73c9bcdcee00f5058095f15295
Damilola <damilolaoni07@gmail.com>
2025-06-27 15:31:40 +0100

    datepill update

--- src/components/DatePill.jsx
+++ src/components/DatePill.jsx
@@ -1,11 +1,10 @@
 const DatePill = () => {
     return (
       <div className="date-pill mt-12 flex justify-center px-4">
-        <div className="flex flex-col md:flex-row items-center justify-center bg-[#101828] rounded-[60px] lg:rounded-[100%] py-6 px-4 gap-6 shadow-lg 
-                        w-full max-w-[1312px] h-auto lg:h-[283px]">
+        <div className="flex flex-col md:flex-row items-center justify-center py-6 px-4 gap-6 w-full max-w-[1312px] h-auto lg:h-[283px]">
   
           <div className="text-center now-open space-y-4">
-            <p className="text-white text-[24px] lg:text-[30px] font-semibold font-poppins">NOW OPEN</p>
+            <p className="text-[#4E88E0] text-[24px] lg:text-[30px] font-semibold font-poppins">Now Open</p>
             <div className="!bg-[#07937C] px-3 sm:px-0 border-[6px] border-white text-white text-[24px] lg:text-[30px] font-poppins font-semibold 
                             w-full max-w-[316px] h-[96px] lg:h-[112px] rounded-2xl flex justify-center items-center mx-auto sm:w-[316px]">
               Register Now !!!
@@ -13,7 +12,7 @@ const DatePill = () => {
           </div>
   
           <div className="text-center may-30th space-y-4">
-            <p className="text-white text-[24px] lg:text-[30px] font-semibold font-poppins">MAY 30th 2025</p>
+            <p className="text-[#4E88E0] text-[24px] lg:text-[30px] font-semibold font-poppins">July 18th </p>
             <span className="!bg-[#FF0000CC] px-3 sm:px-0 border-[6px] border-white text-white text-[24px] lg:text-[30px] font-poppins font-semibold 
                             w-full max-w-[316px] h-[96px] lg:h-[112px] rounded-2xl flex justify-center items-center mx-auto sm:w-[340px]">
               Registration closes
@@ -21,7 +20,7 @@ const DatePill = () => {
           </div>
   
           <div className="text-center space-y-4">
-            <p className="text-white text-[24px] lg:text-[30px] font-semibold font-poppins">JUNE 2nd 2025</p>
+            <p className="text-[#4E88E0] text-[24px] lg:text-[30px] font-semibold font-poppins">July 23rd</p>
             <span className="!bg-[#4E88E0] px-3 sm:px-0 border-[6px] border-white text-white text-[24px] lg:text-[30px] font-poppins font-semibold 
                             w-full max-w-[316px] h-[96px] lg:h-[112px] rounded-2xl flex justify-center items-center mx-auto sm:w-[316px]">
               Cohort starts
