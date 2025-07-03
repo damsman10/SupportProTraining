634fba2d75f7f3eb43075d9cb1d38e16c63af581
Damilola <damilolaoni07@gmail.com>
2025-07-03 10:36:23 +0100

    questions to chat

--- src/components/AnyQuestions.jsx
+++ src/components/AnyQuestions.jsx
@@ -1,11 +1,19 @@
+import { ChatBubbleLeftRightIcon } from '@heroicons/react/24/outline';
+
 const AnyQuestions = () => {
   return (
     <div className="text-center py-12 flex flex-col md:flex-row items-center justify-center gap-6 md:gap-8 my-8 px-4">
       <h3 className="text-[28px] md:text-[40px] font-raleway font-bold mb-4 md:mb-0">
         Any questions?
       </h3>
-      <button className="bg-[#EA2821] cursor-pointer w-[160px] h-[48px] md:w-[162px] md:h-[51px] rounded-[30px] text-white py-2 px-6 hover:bg-red-700 transition font-raleway font-bold text-[18px] md:text-[20px]">
-        Click here
+      <button
+        onClick={() => {
+          window.open('https://wa.me/2349139981335?text=Hi%20Salesplat%2C%20I%E2%80%99ll%20love%20to%20Enquire%20about%20the%20HR%20Training%20Program%20', '_blank');
+        }}
+        className="flex items-center justify-center gap-2 w-[200px] transition bg-green-600 hover:bg-green-700 text-white font-raleway text-[18px] sm:text-[20px] h-[50px] px-2 py-2 rounded-[10px] cursor-pointer"
+      >
+        <ChatBubbleLeftRightIcon className="w-6 h-6" />
+        Chat with a Rep
       </button>
     </div>
   );
